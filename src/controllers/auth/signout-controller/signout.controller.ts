f1d30885d13360bbb18eed25734d952cf6954404
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-02-16 22:37:01 -0300

    feature: add signout feature

--- src/controllers/auth/signout-controller/signout.controller.ts
+++ src/controllers/auth/signout-controller/signout.controller.ts
@@ -2,9 +2,7 @@ import {Request, Response} from "express";
 
 export async function signoutController(_req: Request, res: Response): Promise<Response>{
     try{
-        console.log("Antes de fazer a req" + _req.headers.cookie)
         res.clearCookie("token").send();
-        console.log("Depois de fazer a req" + _req.headers.cookie)
         return res.status(204);
     }catch (err){
         console.log(err);
